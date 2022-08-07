e1ea5fe2288ccead37575e0538ac6ae69521cf0e
Ronaldo <109868127+R0NALD00@users.noreply.github.com>
2022-08-07 20:42:14 -0300

    Calcular data de nascimento

---
+++ Calcular Data de Nascimento.py
@@ -0,0 +1,8 @@
+nome = input("Qual seu nome? ")
+idade =  input("Quantos anos você tem? ")
+ano_nascimento = 2022-int(idade)
+
+print(f'{nome} tem {idade} anos e '
+      f'nasceu em {ano_nascimento}.')
+
+Final = input('Pressione enter para fechar. ')
